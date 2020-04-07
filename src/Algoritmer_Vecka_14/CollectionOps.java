7211b9f8c53176df79ac66ad39bdcfec0ff60c25
Oliver Dizdarevic <oliver@dizdarevic.se>
2020-04-08 00:32:49 +0200

    2(a) is done

--- src/Algoritmer_Vecka_14/CollectionOps.java
+++ src/Algoritmer_Vecka_14/CollectionOps.java
@@ -1,19 +1,37 @@
 package Algoritmer_Vecka_14;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 public class CollectionOps {
     public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList<String>();
+        list.add("Oliver");
+        list.add("Alex");
+        list.add("Igor");
 
+        print(list);
     }
 
     /*---metoder---*/
 
-    public static <T> void print(Collection<T> hiya){
-        var iterator = hiya.iterator();
-
-        while(iterator.hasNext() == true){
-            
+    public static <T> void print(Collection<T> l) {
+        var iterator = l.iterator();
+        System.out.print("[");
+        while (iterator.hasNext()) {
+            System.out.print(iterator.next());
+            if (iterator.hasNext() == true){
+                System.out.print(",");
+            }
         }
+        System.out.print("]");
     }
+
+    public static <T> List<T> reverse(List<T> l){
+        //TODO: hela denna
+        return l;
+    }
+
 }
+
