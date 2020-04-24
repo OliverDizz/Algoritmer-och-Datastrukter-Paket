05008d56b9d787b448ffc453136eff7fb96ff7c2
Oliver Dizdarevic <oliver@dizdarevic.se>
2020-04-24 17:32:08 +0200

    Week 13 finished
    Tested, some places needs to be commented, also change language to english

--- src/Algoritmer_Vecka_13/Klasslista_uppgift.java
+++ src/Algoritmer_Vecka_13/Klasslista_uppgift.java
@@ -2,41 +2,47 @@ package Algoritmer_Vecka_13;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Klasslista_uppgift {
     public static void main(String[] args) throws FileNotFoundException {
         ArrayList<String> list = new ArrayList<>();
-        File fil = new File("/Users/oliverdizdarevic/IdeaProjects/Algoritmer och Datastrukturer Paket/src/Algoritmer_Vecka_13/klasslista.txt");
+        File fil = new File("/Users/oliverdizdarevic/IdeaProjects/Algoritmer och Datastrukturer Paket/src/Algoritmer_Vecka_13/Klasslista.txt");
         Scanner input = new Scanner(fil);
 
         //fills ArrayList with names from file
-        while(input.hasNextLine())  {
-            list.add(input.nextLine());
+        while (input.hasNextLine()) {
+            addToList(list, input.nextLine());
+
         }
         input.close();
+        listPrint(list);
+        System.out.println(isSorted(list));
 
+
+         /*
+        //First test
         ArrayList<String> test = new ArrayList<>();
-
         test.add("A");
         test.add("B");
         test.add("D");
-
         addToList(test, "C");
         listPrint(test);
+        System.out.println(isSorted(test));
+        */
+
 
     }
 
     /*---METHODS---*/
-    public static String findName (ArrayList<String> list, int index ){
+    public static String findName(ArrayList<String> list, int index) {
         return (String) list.get(index);
     }
 
-    public static boolean deleteName(ArrayList<String> list, String name){
+    public static boolean deleteName(ArrayList<String> list, String name) {
         for (int i = 0; i < list.size(); i++) {
-            if (list.get(i).equals(name.toLowerCase())){
+            if (list.get(i).equals(name.toLowerCase())) {
                 list.remove(i);
                 return true;
             }
@@ -44,10 +50,10 @@ public class Klasslista_uppgift {
         return false;
     }
 
-    public static boolean addToList(ArrayList<String> list, String name){
+    public static boolean addToList(ArrayList<String> list, String name) {
         for (int i = 0; i < list.size(); i++) {
             String item = list.get(i);
-            if (item.compareToIgnoreCase(name) > 0 ){
+            if (item.compareToIgnoreCase(name) > 0) {
                 list.add(i, name);
                 return true;
             }
@@ -56,10 +62,20 @@ public class Klasslista_uppgift {
         return false;
     }
 
-    public static void listPrint(ArrayList<String> list){
+    public static void listPrint(ArrayList<String> list) {
         for (int i = 0; i < list.size(); i++) {
             System.out.println(list.get(i));
         }
     }
 
+    public static boolean isSorted(ArrayList<String> list) {
+        for (int i = 0; i < list.size() - 1; i++) {
+            String item_1 = list.get(i);
+            String item_2 = list.get(i + 1);
+            if (item_1.compareToIgnoreCase(item_2) > 0) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
