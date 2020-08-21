5e861737bafba3cdf9f247fe0f3141e1f3d94f78
Oliver Dizdarevic <oliver@dizdarevic.se>
2020-08-21 19:08:56 +0200

    arraylist_main code

--- src/Algoritmer_Vecka_14/ArrayList_Main.java
+++ src/Algoritmer_Vecka_14/ArrayList_Main.java
@@ -1,4 +1,26 @@
 package Algoritmer_Vecka_14;
 
-public class ArrayListMain {
+import java.util.Iterator;
+
+public class ArrayList_Main {
+    public static void main(String[] args) {
+        MyArrayList<String> test = new MyArrayList<String>();
+        test.add("Oliver");
+        test.add("Igor");
+        test.add("Alex");
+        test.add("Fabian");
+
+        test.print();
+        System.out.println(test.size());
+        System.out.println(test.contains("Fabian"));
+        System.out.println(test.get(2));
+        System.out.println(test.indexOf("Alex"));
+
+        test.remove("Fabian");
+        test.print();
+
+
+
+
+    }
 }
